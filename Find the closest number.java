1c443fe11c88c2f434fdbb5cc3170b8ce7c7fdf1
Suvadip Pramanik <133400651+PS-003R32@users.noreply.github.com>
2024-05-19 09:36:02 +0530

    Create Find the closest number.java

---
+++ Find the closest number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int findClosest(int n, int k, int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int v = 0;
+        for(int i=0; i<n; i++){
+            int a = Math.abs(arr[i] - k);
+            if(a <= min){
+                min = a;
+                if(arr[i] > v){
+                    v = arr[i];
+                }
+            }
+        }
+        return v;
+    }
+}
